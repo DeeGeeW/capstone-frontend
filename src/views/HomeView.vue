3f8e188156788f4de753340ed9676e8a3df38795
DeeGeeWest <DeeGeeWest@gmail.com>
2022-04-28 18:11:42 -0400

    basic index function on home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Ready for an ADVENTURE?!",
+      birds: [],
+      locations: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexBirds();
+  },
+  methods: {
+    indexBirds: function () {
+      axios.get("/birds").then((response) => {
+        console.log(response.data);
+        this.birds = response.data;
+      });
+    },
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="bird in birds" v-bind:key="bird.c_name">
+      <h3>{{ bird.c_name }}</h3>
+      <p>Scientific Name: {{ bird.s_name }}</p>
+    </div>
   </div>
 </template>
 
