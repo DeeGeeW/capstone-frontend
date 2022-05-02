3daa8a4bdbfe0bae6dd3fbe604312ac90f81d824
DeeGeeWest <DeeGeeWest@gmail.com>
2022-05-02 17:35:49 -0400

    not much unsuccessfull v for comments on bird page

--- src/views/BirdView.vue
+++ src/views/BirdView.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="home">
+    <!-- <div v-bind:style="background-image url(bird.background_image)" v-bind:key="bird.id"> -->
     <h1>{{ bird.c_name }}</h1>
     <!-- <p>c_name: {{ bird.c_name }}</p> -->
     <img v-bind:src="bird.image_url" v-bind:alt="bird.c_name" style="max-width: 250px" />
     <p>s_name: {{ bird.s_name }}</p>
     <p>description: {{ bird.description }}</p>
+    <!-- <p>{{ bird.comments[0].comment_text }}</p> -->
     <!-- <div class="container">
       <p v-for="bird.comment in comments", v-text"comment"></p>
     </div> -->
     <router-link to="/" style="margin-right: 10px">Back to all birds</router-link>
+    <div class="comment-box" v-for="comment in comments" v-bind:key="comment.comment_id">
+      <p>{{ bird.comment.comment_text }}</p>
+      <p>{{ bird.c_name }}</p>
+      <p>{{ bird.comments[0].comment_text }}</p>
+      <p>{{ comments[0].lat }}</p>
+      <p>{{ comments.long }}</p>
+    </div>
     <!-- <router-link v-bind:to="`/birds/${bird.id}/edit`" style="margin-right: 10px">Edit bird</router-link> -->
     <!-- <button v-on:click="destroybird()">Delete</button> -->
   </div>
+  <!-- </div> -->
 </template>
 
 <script>
@@ -20,13 +30,28 @@ export default {
   data: function () {
     return {
       bird: {},
+      // comment: {},
     };
   },
   created: function () {
     axios.get(`/birds/${this.$route.params.id}`).then((response) => {
       this.bird = response.data;
+      console.log(this.bird);
     });
   },
+  // function(index(num)) {
+  //   while (num < bird.comments.length()) {
+  //   return num;
+  //   num++;
+  //   }
+  // }
+  // console.log(index(0));
+  // function (comments_i) {
+  //   while (comments_i < bird.comments.length){
+  //     return comments_i;
+  //   }
+  // }
+  // console.log(0);
   // methods: {
   //   destroybird() {
   //     axios.delete(`/birds/${this.bird.id}`).then((response) => {
@@ -42,4 +67,8 @@ export default {
 /* div {
   background-image: url(bird.background_url);
 } */
+div .comment-box {
+  border-style: inset;
+  color: blue;
+}
 </style>
