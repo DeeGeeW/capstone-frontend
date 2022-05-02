5b9bf4fa9cf12c4fb86dc3e06770a53601585445
DeeGeeWest <DeeGeeWest@gmail.com>
2022-05-02 18:25:27 -0400

    got something rolling

--- src/views/BirdView.vue
+++ src/views/BirdView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    <!-- <div v-bind:style="background-image url(bird.background_image)" v-bind:key="bird.id"> -->
+  <div class="home" v-bind:style="{ backgroundImage: `url(${bird.background_url})` }">
+    <div class="test" v-bind:style="{ backgroundImage: `url(${bird.background_url})` }" v-bind:key="bird.id"></div>
     <h1>{{ bird.c_name }}</h1>
     <!-- <p>c_name: {{ bird.c_name }}</p> -->
     <img v-bind:src="bird.image_url" v-bind:alt="bird.c_name" style="max-width: 250px" />
@@ -11,12 +11,12 @@
       <p v-for="bird.comment in comments", v-text"comment"></p>
     </div> -->
     <router-link to="/" style="margin-right: 10px">Back to all birds</router-link>
-    <div class="comment-box" v-for="comment in comments" v-bind:key="comment.id">
-      <p>{{ bird.comment.comment_text }}</p>
+    <div class="comment-box" v-for="comment in bird.comments" v-bind:key="comment.id">
+      <p>{{ comment.comment_text }}</p>
       <p>{{ bird.c_name }}</p>
-      <p>{{ bird.comments[0].comment_text }}</p>
+      <p>User ID:{{ comment.user_id }}</p>
       <p>{{ comment.lat }}</p>
-      <p>{{ comments.long }}</p>
+      <p>{{ comment.long }}</p>
     </div>
     <!-- <router-link v-bind:to="`/birds/${bird.id}/edit`" style="margin-right: 10px">Edit bird</router-link> -->
     <!-- <button v-on:click="destroybird()">Delete</button> -->
@@ -33,7 +33,8 @@ export default {
       // comment: {},
     };
   },
-  created: function () {
+  mounted: function () {
+    console.log(this.$route.params.id);
     axios.get(`/birds/${this.$route.params.id}`).then((response) => {
       this.bird = response.data;
       console.log(this.bird);
@@ -70,5 +71,8 @@ export default {
 div .comment-box {
   border-style: inset;
   color: blue;
+}
+.test {
+  height: 100px;
 }
 </style>
