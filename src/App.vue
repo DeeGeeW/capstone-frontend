fc644622f5b2e58580fc8b8b620f219e39470b6c
DeeGeeWest <DeeGeeWest@gmail.com>
2022-08-08 21:32:37 -0400

    egg back out look into keeping on

--- src/App.vue
+++ src/App.vue
@@ -206,7 +206,7 @@ nav a.router-link-exact-active {
   min-width: 100%;
   color: goldenrod;
   padding-bottom: 800px;
-  cursor: url("http://www.rw-designer.com/icon-image/12294-24x24x8.png"), auto;
+  /* cursor: url("http://www.rw-designer.com/icon-image/12294-24x24x8.png"), auto; */
 }
 .home-bg h1 {
   border-style: inset;
