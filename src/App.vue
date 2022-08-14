036181df0be3599fb3bcf4f52a96200671153156
DeeGeeWest <DeeGeeWest@gmail.com>
2022-08-14 13:00:59 -0400

    images in vue app

--- src/App.vue
+++ src/App.vue
@@ -206,7 +206,7 @@ nav a.router-link-exact-active {
   min-width: 100%;
   color: goldenrod;
   padding-bottom: 800px;
-  cursor: url("images/12294-24x24x8 (2).png"), pointer, auto;
+  cursor: url("images/12294-24x24x8 (2).png"), auto;
 }
 .home-bg h1 {
   border-style: inset;
